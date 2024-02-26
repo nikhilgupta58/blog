304bc2e0fea5c203e851da9362fc1971edfbee7c
Nikhil <nikhil@tartanhq.com>
2024-02-26 19:14:59 +0530

    Built Indivisual Blog page too - mocked for now

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,5 +1,24 @@
-export default function Blog(){
-    return(
-        <></>
-    )
+import AppLayout from "../../components/AppLayout";
+
+const mock = {
+  id: "sdkfnk-asfs-df-dsfo-fds-d",
+  title: "A Devotee",
+  author: "Dan Carlie",
+  content:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque accusamus vitae praesentium maxime possimus ipsam. Nulla, tempora deleniti autem dolores, fugit velit quam quaerat nostrum alias natus saepe enim!",
+};
+
+export default function Blog() {
+  const data = mock;
+  return (
+    <AppLayout>
+      <div className="p-4 gap-[32px] flex flex-col">
+        <div>
+          <h1 className="text-[40px] font-bold">{data.title}</h1>
+          <h6 className="text-[20px] font-semibold">by {data.author}</h6>
+        </div>
+        <p className="text-[16px]">{data?.content}</p>
+      </div>
+    </AppLayout>
+  );
 }
